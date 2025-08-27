da0dc126a6a99c6d635261c66f0da8ba34775bcd
Lady-Marianne <mariana.andrea.lois@gmail.com>
2025-08-27 19:26:06 -0300

    Cambios manuales.

---
+++ src/main/java/learning/platform/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package learning.platform.repository;
+import learning.platform.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
