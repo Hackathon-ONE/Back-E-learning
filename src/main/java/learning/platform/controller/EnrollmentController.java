c60e74945cd382eb7acca3ec7533c0096ebea80e
Francisco <francisco.vera.95@gmail.com>
2025-08-27 15:34:09 -0600

    guarda cambios

--- src/main/java/learning/platform/controller/EnrollmentController.java
+++ src/main/java/learning/platform/controller/EnrollmentController.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/enrollment")
+@RequestMapping("/api/enrollment")
 public class EnrollmentController {
 
     @Autowired
