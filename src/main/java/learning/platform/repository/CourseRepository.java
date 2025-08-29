fe8ee00be3b5143749bb430d142d20fc2225fdbb
Francisco <francisco.vera.95@gmail.com>
2025-08-29 16:27:32 -0600

    save changes

--- src/main/java/learning/platform/repository/CourseRepository.java
+++
@@ -1,7 +0,0 @@
-package learning.platform.repository;
-
-import learning.platform.entity.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-}
